0099ea43f85cf5dcab2def2a6776a68fdce98ee5
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-09-02 21:30:33 +0300

    created appbar and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AppBar, Toolbar, Container, IconButton, Typography, Button, Box } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppBar position="fixed">
+        <Container fixed>
+          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <IconButton
+                edge='start'
+                color="inherit"
+                aria-label="menu"
+
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                component='p'
+                variant="h6"
+              >
+                Web developer block
+              </Typography>
+            <Box >
+              <Button
+                color="inherit"
+                variant="outlined"
+                sx={{marginRight: '16px'}}
+              >
+                Log in
+              </Button>
+              <Button
+                color="secondary"
+                variant="contained"
+              >
+                Sign up
+              </Button>
+            </Box>
+
+          </Toolbar>
+        </Container>
+      </AppBar>
     </>
+
   )
 }
 
-export default App
+export { App } 
